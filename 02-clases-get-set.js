9968a5a5ff25cdea5623dd964b02628636327927
Tobias <tobiasvega1210@gmail.com>
2025-08-09 12:45:54 -0300

    utilización de get y set

--- 02-clases-get-set.js
+++ 02-clases-get-set.js
@@ -1,29 +1,49 @@
 class CuentaBancaria {
 
-  titular;
-  #saldoInicial;
-  
+  #saldo;
 
   constructor(titular, saldoInicial){
-    this.#saldoInicial = saldoInicial;
+
+    if (saldoInicial < 0) {
+      return 'El saldo no puede ser negativo';
+    }
+    this.#saldo = saldoInicial;
     this.titular = titular;
   }
 
   get saldo() {
-    return `Saldo actual: ${this.#saldoInicial}`;
+    return this.#saldo;
   }
 
   set saldo(nuevoSaldo) {
-    if (nuevoSaldo < 0) {
-      console.log('El saldo no puede ser negativo');
-      return;
+    if (nuevoSaldo >= 0) {
+      this.#saldo = nuevoSaldo;
+      console.log(`Nuevo saldo: ${this.#saldo}`)
     } else {
-      this.#saldoInicial = nuevoSaldo;
-      return;
+      console.log('El saldo no puede ser negativo');
+    }
+  }
+
+  depositar(monto) {
+    this.#saldo += monto;
+    console.log(`Se ha depositado $${monto}`);
+  }
+
+  extraer(monto) {
+    if (this.#saldo >= monto) {
+      this.#saldo -= monto;
+      console.log(`Se ha retirado $${monto}`)
+    } else {
+      console.log('Fondos insuficientes')
     }
   }
 }
 
 const cuenta = new CuentaBancaria('Juan', 1000);
 
+console.log(cuenta.saldo);
+cuenta.depositar(700);
+console.log(cuenta.saldo)
+cuenta.saldo = 2000;
+cuenta.extraer(500);
 console.log(cuenta.saldo);
