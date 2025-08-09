3e63f09996197b350983aea60740eec5e329d57d
Tobias <tobiasvega1210@gmail.com>
2025-08-09 12:17:47 -0300

    style: :loud_sound: mensajes más descriptivos

--- 01-funcion-constructora.js
+++ 01-funcion-constructora.js
@@ -1,34 +1,35 @@
-const CuentaBancariaPrototype = {
+const cuentaBancariaPrototype = {
   depositar(monto) {
     this.saldoInicial += monto;
-    console.log()
+    console.log(`Se ha depositado $${monto}`);
   },
+
   extraer(monto) {
     if (this.saldoInicial > monto) {
       this.saldoInicial -= monto;
+      console.log(`Se ha retirado $${monto}`)
     } else {
-      console.log("Fondos insuficientes")
+      console.log('Fondos insuficientes')
     }
   },
+
   consultarSaldo() {
-    console.log(`Saldo actual: ${this.saldoInicial}`);
+    console.log(`Saldo actual: ${this.saldoInicial}`)
   }
 }
 
 function CuentaBancaria(titular, saldoInicial) {
-  this.titulartitular = titular;
+  this.titular = titular;
   this.saldoInicial = saldoInicial;
 }
 
-Object.assign(CuentaBancaria.prototype, CuentaBancariaPrototype);
+Object.assign(CuentaBancaria.prototype, cuentaBancariaPrototype);
 
 const cuenta = new CuentaBancaria('Juan', 1000);
 
+cuenta.consultarSaldo();
 cuenta.depositar(1000);
 cuenta.consultarSaldo();
-
-cuenta.extraer(1500);
-
+cuenta.extraer(500);
 cuenta.consultarSaldo();
-
-cuenta.extraer(1000);
+cuenta.extraer(2000);
